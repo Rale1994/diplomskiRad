d735eb09d4e9878b8085a8339d1cb8e3e8912229
Rale1994 <golrale3@gmail.com>
2020-08-23 11:12:24 +0200

    Lines starting

---
+++ SeminarskiServer2020/src/logika/Kontroler.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logika;
+
+/**
+ *
+ * @author Rados
+ */
+public class Kontroler {
+
+    private static Kontroler instanca;
+
+    public Kontroler() {
+    }
+
+    public static Kontroler getInstanca() {
+        if (instanca == null) {
+            instanca = new Kontroler();
+        }
+        return instanca;
+    }
+
+}
